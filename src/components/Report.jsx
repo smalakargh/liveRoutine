38af7e3a42b5b9a3c3d4f7ed20a19231c8c96625
smalakargh <supriyomic@gmail.com>
2025-08-13 19:35:41 +0530

    Report Bug Link  #Added

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -4,7 +4,8 @@ import { FaBug } from 'react-icons/fa6'
 function Report() {
   return (
     <>
-      <div className='w-[40px] h-[40px] rounded-full bg-purple-400 flex justify-center items-center fixed bottom-9 right-3 text-white text-[19px]'><FaBug /></div>
+    <a href="https://forms.gle/TAsR3WfimTTN4gCv5"><div className='w-[40px] h-[40px] rounded-full bg-purple-400 flex justify-center items-center fixed bottom-9 right-3 text-white text-[19px]'><FaBug /></div>
+</a>
     </>
   )
 }

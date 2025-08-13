7426361f9ba561bf44bba1a484fb6e92f3f8fa12
smalakargh <supriyomic@gmail.com>
2025-08-13 22:07:00 +0530

    Schedule #Updated

--- src/data/schedule.js
+++ src/data/schedule.js
@@ -1,33 +1,39 @@
 export const schedule = {
   Monday: [
-     { start: "09:50", end: "10:40", subject: "Data Structure & Algorithm", code: "PCC-CS301", teacher: "HB", room: "6009", type: "class" },
-     { start: "10:40", end: "11:30", subject: "Mathematics-III (Differential Calculus)", code: "BSC-301", teacher: "KD", room: "6009", type: "class" },
-     { start: "11:30", end: "12:20", subject: "Analog & Digital Electronics Lab", code: "ESC-391", teacher: "ECE Dept.", room: "Lab-XX", type: "lab", group: "G1" },
-     { start: "13:10", end: "14:00", subject: "Break", type: "break" },
+     { start: "10:40", end: "12:20", subject: "Computer Organization", code: "PCC-CS302", teacher: "LS", room: "Lab-17", type: "class" },
+     { start: "12:20", end: "14:50", subject: "Analog & Digital Electronics Lab", code: "ESC-391", teacher: "ECE Dept.", room: "Lab-XX", type: "lab" },
+     { start: "14:50", end: "16:30", subject: "Economics for Engineers (Humanities-II)", code: "HSMC-301", teacher: "AC", room: "6009", type: "class" },
+   //   { start: "13:10", end: "14:00", subject: "Break", type: "break" },
+],
+
+Tuesday: [
+   { start: "09:00", end: "12:20", subject: "Data Science Lab using Python", code: "PCC- DS391", teacher: "AD, UR, AS", room: "Lab-13", type: "class" },
+   { start: "12:20", end: "13:10", subject: "Data Structure & Algorithm", code: "PCC-CS301", teacher: "HB", room: "Lab-13", type: "class" },
+   { start: "13:10", end: "14:00", subject: "Break", type: "break" },
+   { start: "14:00", end: "14:50", subject: "Introduction to Data Science", code: "PCC-DS301", teacher: "AD", room: "8012", type: "class" },
+   { start: "14:50", end: "15:40", subject: "Computer Organization", code: "PCC-CS302", teacher: "LS", room: "8012", type: "class" },
+   { start: "15:40", end: "16:30", subject: "Economics for Engineers", code: "HSMC-301", teacher: "AC", room: "8012", type: "class" },
+],
+
+Wednesday: [
+   { start: "09:50", end: "11:30", subject: "Data Structure & Algorithm", code: "PCC-CS301", teacher: "HB", room: "6009", type: "class" },
+   { start: "11:30", end: "13:10", subject: "Mathematics-III (Differential Calculus)", code: "BSC-301", teacher: "KD", room: "6009", type: "class" },
+   { start: "13:10", end: "14:00", subject: "Break", type: "break" },
+   { start: "14:00", end: "17:20", subject: "Computer Organization Lab Grp-1", code: "PCC-CS392", teacher: "LS, RC, SS3", room: "6001", type: "lab", group: "G1" },
    ],
    
-   Tuesday: [
-      { start: "09:50", end: "10:40", subject: "Data Structure & Algorithm", code: "PCC-CS301", teacher: "HB", room: "Lab-13", type: "class" },
-      { start: "14:00", end: "14:50", subject: "Introduction to Data Science", code: "PCC-DS301", teacher: "AD", room: "8012", type: "class" },
-      { start: "14:50", end: "15:40", subject: "Computer Organization", code: "PCC-CS302", teacher: "LS", room: "8012", type: "class" },
-      { start: "15:40", end: "16:30", subject: "Economics for Engineers", code: "HSMC-301", teacher: "AC", room: "8012", type: "class" },
-   ],
-   
-   Wednesday: [
-      { start: "09:50", end: "10:40", subject: "Data Structure & Algorithm", code: "PCC-CS301", teacher: "HB", room: "6009", type: "class" },
-      { start: "10:40", end: "11:30", subject: "Mathematics-III (Differential Calculus)", code: "BSC-301", teacher: "KD", room: "6009", type: "class" },
-      { start: "14:00", end: "14:50", subject: "Computer Organization Lab", code: "PCC-CS392", teacher: "LS, RC, SS3", room: "6001", type: "lab", group: "G1" },
-  ],
+Thursday: [
+   { start: "09:00", end: "12:20", subject: "Computer Organization Lab Grp-2", code: "PCC-CS392", teacher: "LS, SS3", room: "6001", type: "class" },
+   { start: "12:20", end: "13:10", subject: "Analog & Digital Electronics", code: "ESC-301", teacher: "ASK (ECE Dept.)", room: "6010", type: "class" },
+   { start: "13:10", end: "14:00", subject: "Break", type: "break" },
+   { start: "14:00", end: "14:50", subject: "Introduction to Data Science", code: "PCC-DS301", teacher: "AD", room: "7009", type: "class" },
+   { start: "14:50", end: "15:40", subject: "Analog & Digital Electronics", code: "ESC-301", teacher: "ASK (ECE Dept.)", room: "7009", type: "class" },
+   { start: "15:40", end: "16:30", subject: "Mathematics-III (Differential Calculus)", code: "BSC-301", teacher: "KD", room: "7009", type: "class" },
+],
 
-  Thursday: [
-     { start: "10:40", end: "11:30", subject: "Analog & Digital Electronics", code: "ESC-301", teacher: "ASK (ECE Dept.)", room: "6010", type: "class" },
-     { start: "14:00", end: "14:50", subject: "Introduction to Data Science", code: "PCC-DS301", teacher: "AD", room: "7009", type: "class" },
-     { start: "14:50", end: "15:40", subject: "Analog & Digital Electronics", code: "ESC-301", teacher: "ASK (ECE Dept.)", room: "7009", type: "class" },
-     { start: "15:40", end: "16:30", subject: "Mathematics-III (Differential Calculus)", code: "BSC-301", teacher: "KD", room: "7009", type: "class" },
-  ],
-
-  Friday: [
-     { start: "09:50", end: "10:40", subject: "Data Structure & Algorithms Lab", code: "PCC-CS391", teacher: "SS2, SP", room: "Lab-13", type: "lab", group: "G1" },
-     { start: "14:50", end: "15:40", subject: "Data Structure & Algorithms Lab", code: "PCC-CS391", teacher: "SS3, SP", room: "Lab-11", type: "lab", group: "G2" },
+Friday: [
+   { start: "09:50", end: "13:10", subject: "Data Structure & Algorithms Lab Grp-1", code: "PCC-CS391", teacher: "SS2, SP", room: "Lab-13", type: "lab", group: "G1" },
+   { start: "13:10", end: "14:00", subject: "Break", type: "break" },
+   { start: "14:00", end: "17:20", subject: "Data Structure & Algorithms Lab Grp-2", code: "PCC-CS391", teacher: "SS3, SP", room: "Lab-11", type: "lab", group: "G2" },
   ],
 };

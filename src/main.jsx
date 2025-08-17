9bca6eec0dc1e53ec8a40d53ddd0ac891882d718
smalakargh <supriyomic@gmail.com>
2025-08-17 22:47:13 +0530

    vercel Insight #Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './App.jsx'
 
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SpeedInsights />
     <App />
   </StrictMode>,
 )

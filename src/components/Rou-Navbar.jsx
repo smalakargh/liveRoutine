21e9615afacb6f3799f6c8d08645efa31aa35a6b
smalakargh <supriyomic@gmail.com>
2025-08-17 12:26:32 +0530

    Github Star #BtnUpdated

--- src/components/Rou-Navbar.jsx
+++ src/components/Rou-Navbar.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { motion } from "motion/react"
 import { FaGithub, FaStar } from 'react-icons/fa6'
+import { TiStarFullOutline } from 'react-icons/ti'
 import { WiTime12 } from 'react-icons/wi'
 
 function navbar() {
   return (
-    <main className='poppins bg-[#999999] text-white w-full rounded-4xl border-1 border-purple-300'>
+    <main className='poppins bg-[#989898] text-white w-full rounded-4xl border-1 border-purple-300'>
       <div className='flex justify-between px-3 py-2 items-center'>
         <div className='caveat text-[35px] font-[600] pr-3 flex items-center gap-1 '><WiTime12 className='text-[24px] animate-spin' />liveRoutine</div>
-        <a href="https://github.com/smalakargh/liveRoutine" className='flex relative cursor-pointer items-center font-[500] gap-1 px-2 py-1 rounded-lg'><FaStar className='text-[30px] text-amber-300 animate-ping absolute' /> <FaGithub className='text-[30px] text-black' />
-</a>
+        {/* <a href="https://github.com/smalakargh/liveRoutine" className='flex relative cursor-pointer items-center font-[500] gap-1 px-2 py-1 rounded-lg'><FaStar className='text-[30px] text-amber-300 animate-ping absolute' /> <FaGithub className='text-[30px] text-black' />
+</a> */}
+        <a href="https://github.com/smalakargh/liveRoutine">
+        <motion.div className='cursor-pointer bg-gradient-to-r border-1 from-purple-500 via-purple-400 to-blue-500 rounded-full px-4 py-2 font-[600] flex items-center gap-1 text-[12px] md:text-[16px]'
+        animate={{ boxShadow: "2px 4px #616161" }}> Star On Github
+          <div className='px-2 bg-black rounded-full py-1 border-1 text-[16px] text-amber-300'><TiStarFullOutline className='absolute' /><TiStarFullOutline className='animate-ping' /></div>
+        </motion.div>
+        </a>
       </div>
     </main>
   )
